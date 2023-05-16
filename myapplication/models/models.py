ead29c0d788c930f7e2458acb0af7e0da85055d1
Jana Pascual <jacpascual@gmail.com>
2023-05-16 16:35:41 -0400

    Add to database models and creates relationship between User and Todo models

--- myapplication/models/models.py
+++ myapplication/models/models.py
@@ -1,5 +1,5 @@
 from myapplication.db import Base, init_db
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(Base):
@@ -8,14 +8,33 @@ class User(Base):
     username = Column(String(75), index=True)
     email = Column(String(100), index=True, unique=True)
     password_hash = Column(String(128))
+    posts = Column(Integer, ForeignKey("todos"))
 
-def __init__(self, username, email, password):
-    self.usernamne = username
-    self.email = email
-    self.password_hash = generate_password_hash(password)
+    def __init__(self, username, email, password, posts):
+        self.usernamne = username
+        self.email = email
+        self.password_hash = generate_password_hash(password)
+        self.posts = posts
 
-def verify_password(self, password):
-    return check_password_hash(self.password_hash, password)
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
-def __repr__(self):
-    return f'<User {self.name!r}>'
+    def __repr__(self):
+        return f'<User {self.name!r}>'
+    
+    
+class ToDo(Base):
+    __tablename__ = "todos"
+    id = Column(Integer, primary_key=True)
+    owner = Column(Integer, ForeignKey("user.id"), nullable=False)
+    description = Column(String(128), nullable=False)
+    date = Column(DateTime.date())
+
+    def __init__(self, owner, description, date):
+        self.owner = owner
+        self.description = description
+        self.date = date
+
+
+    def __repr__(self):
+        return f'<ToDOs: Description: {self.description} -- Date: {self.date}>'
